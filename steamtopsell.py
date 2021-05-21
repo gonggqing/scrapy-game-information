9140b1ff6c72609a5ba3e67286f5e809a683349c
Cris_Dean <57084033+gonggqing@users.noreply.github.com>
2021-05-21 15:06:14 +0800

    Update steamtopsell.py

--- steamtopsell.py
+++ steamtopsell.py
@@ -1,23 +1,24 @@
 import scrapy
 
-
+# This spider enables me to crawl all the urls from steam.
 class SteamtopsellSpider(scrapy.Spider):
     name = 'steamtopsell'
     allowed_domains = ['store.steampowered.com']
     start_urls = ['https://store.steampowered.com/search/?category1=998&filter=topsellers&page=1']
 
     def parse(self, response):
-
+        # restore all the game urls
         urls = response.xpath("//div[@id='search_resultsRows']/a/@href").extract()
         for url in urls:
             yield {
                 'url': url
             }
-
+        # find next page
         next_page = response.xpath("//div[@class='search_pagination_right']/child::a[last()]/@href").get()
-        print(next_page)
-        print(len(urls))
+        # print(next_page)
+        # it contains 25 urls in one store.steampowered.com page
+        # print(len(urls))
 
         if next_page is not None:
-
+          
             yield response.follow(next_page, callback=self.parse)
